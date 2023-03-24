046047c7190bac5fe4a0205999ba3d94906d3086
Zehra <aydin.fzehra@gmail.com>
2023-03-24 21:55:20 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+const superbowlWin = (record) => {
+  const findW = (element) => {
+    return element.result === "W";
+  };
+  const result = record.find(findW);
+  if (result !== undefined) {
+    return result.year;
+  }
+};
